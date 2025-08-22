3a25be4134af168125ebf8eaa6f69077acaea9a4
Shreyash Chalke <Shreyash1825@users.noreply.github.com>
2025-08-22 23:35:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
+  base: '/bustrackingapp4_vite/',
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') }
   }
